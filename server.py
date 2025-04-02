dcbd55a417f1741d5e9110ea4860c9d2e433dee5
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-04-02 15:45:23 +0530

    changes in server.py

--- server.py
+++ server.py
@@ -110,4 +110,4 @@ async def parse_input(request: ParserRequest):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("app_with_frontend:app", host="0.0.0.0", port=port, reload=True) 
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True) 
